56e744901abc43fbfdfc89fdac8cffc10deb9e2d
Flexy2006 <kudryavtsev25122006@gmail.com>
2024-12-16 15:52:32 +0300

    Add files via upload

--- 16513360/6.py
+++ 16513360/6.py
@@ -1,19 +1,8 @@
-a = open('111.txt')
-q = 0
+a = open('6.txt')
+b = 0
 for i in a:
-    q1m = []
-    q1 = 0
-    q2m = []
-    q2 = 0
-    b = [int(j) for j in i.split()]
-    for k in range(len(b)):
-        if b.count(b[k]) == 1:
-            q1m.append(b[k])
-            q1 += int(b[k])
-        if b.count(b[k]) == 2:
-            q2m.append(b[k])
-            q2 += int(b[k])
-    if len(q1m) == 3 and len(q2m) == 4:
-        if q1/3<q2/4:
-            q+=1
-print(q)
+    c = [int(j) for j in i.split()]
+    d = c.sort()
+    if c[2] ** 2 < c[0] ** 2 + c[1] ** 2:
+        b += 1
+print(b)
--- 16513360/8.py
+++ 16513360/8.py
@@ -1,25 +1,9 @@
-a = open('113.txt')
-q = 0
+a = open('8.txt')
+b = 0
 for i in a:
-    b = [int(j) for j in i.split()]
-    #c = " ".join(str(k) for k in b)
-    #d = c.sort()
-    flag1 = 0
-    flag2 = 0
-    b.sort()
-    #print(b)
-    if int(b[3]) - int(b[2]) == int(b[2]) - int(b[1]) == int(b[1]) - int(b[0]):
-        flag1 = 1
-        #print(b)
-    f = 1
-    w = b[3]
-    b.pop(3)
-    for x in b:
-        f *= x
-    if w**2 > f:
-        flag2 = 1
-        #print(b)
-    if flag1 == flag2 == 1:
-        q += 1
-        #print(b, w, w**2, f)
-print(q)
+    c = [int(j) for j in i.split()]
+    for x in c:
+        if c.count(x) == 3 and len(set(c)) == 4:
+            if (3*x)**2 > (sum(c) - 3*x)**2:
+                b += 1
+print(b//3)
